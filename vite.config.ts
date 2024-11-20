3d0d5f2b880764e0aa518cf5d92463ab0bfa0e9f
Shivab04 <shivachary.900@gmail.com>
2024-11-20 20:05:59 +0530

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+
+export default {
+  server: {
+    host: '0.0.0.0', // Allows access from other devices
+    port: 5173,      // Optional: Customize the port if needed
   },
-});
+};
